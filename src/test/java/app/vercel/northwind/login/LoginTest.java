4d52957323eed41f50f9a56d776006c5724d910f
Israel Alves <delimaalvesisrael@gmail.com>
2026-08-13 12:13:57 -0300

    feat(login): criando novos testes de validação na tela de LoginTest

--- src/test/java/app/vercel/northwind/login/LoginTest.java
+++ src/test/java/app/vercel/northwind/login/LoginTest.java
@@ -13,19 +13,41 @@ public class LoginTest {
     private WebDriver driver;
 
     @BeforeEach
-    public void setUp() { //throws Exception {
+    public void setUp() throws Exception {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
     }
     @Test
-    public void testValidarAcessoSemCredenciais() throws Exception {
+    public void testValidarCamposObgtsVazios() throws Exception {
         driver.get("https://northwind-test-platform.vercel.app/");
         driver.findElement(By.name("email")).click();
         driver.findElement(By.name("password")).click();
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         Assertions.assertTrue(
-                driver.findElement(By.cssSelector("[data-testid=''password-error'']")).
+                driver.findElement(By.cssSelector("[data-testid='password-error']")).
                         isDisplayed(),"Email e senha são obrigatórios");
+    }
+
+    @Test
+    public void testValidarFormatoEmailInvalido () throws Exception {
+        driver.get("https://northwind-test-platform.vercel.app/");
+        driver.findElement(By.name("email")).sendKeys("teste.com");
+        driver.findElement(By.name("password")).sendKeys("Teste@123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Assertions.assertTrue(
+                driver.findElement(By.cssSelector("[data-testid='email-error']")).
+                        isDisplayed(),"Formato de email inválido. Use: nome@dominio.com");
+    }
+
+    @Test
+    public void ValidarUsuarioNaoCadastrado() throws Exception {
+        driver.get("https://northwind-test-platform.vercel.app/");
+        driver.findElement(By.name("email")).sendKeys("qateste@teste.com");
+        driver.findElement(By.name("password")).sendKeys("Teste@123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Assertions.assertTrue(
+                driver.findElement(By.cssSelector("[data-testid='email-error']")).
+                        isDisplayed(),"Usuário não encontrado. Verifique o email ou cadastre-se.");
     }
 
     @AfterEach
